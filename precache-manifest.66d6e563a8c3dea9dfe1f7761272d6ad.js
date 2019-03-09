06b7461299c9b70e41eeb69c9806374c3a7e77ee
strongmuffin <nshusa99@gmail.com>
2019-03-08 20:24:49 -0600

    Updates

---
+++ precache-manifest.66d6e563a8c3dea9dfe1f7761272d6ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0b357b3e21c57fc0088",
+    "url": "/linux-app-store/static/js/runtime~main.d0b357b3.js"
+  },
+  {
+    "revision": "c51d8bc4a5fa27c1d6c8",
+    "url": "/linux-app-store/static/js/main.c51d8bc4.chunk.js"
+  },
+  {
+    "revision": "eb06f89cc283f15caeae",
+    "url": "/linux-app-store/static/js/2.eb06f89c.chunk.js"
+  },
+  {
+    "revision": "c51d8bc4a5fa27c1d6c8",
+    "url": "/linux-app-store/static/css/main.332c2d7e.chunk.css"
+  },
+  {
+    "revision": "39d59d4751a115e52713000718bd9ccd",
+    "url": "/linux-app-store/index.html"
+  }
+];
